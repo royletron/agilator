46561901db736de80d1d12c462330750d5c3c4d1
Darren <darren.royle@oup.com>
2014-01-09 12:09:46 +0000

    Active stories will now show how many stories are owned by a user

--- client/views/layouts/user_sub_menu.js
+++ client/views/layouts/user_sub_menu.js
@@ -1,3 +1,4 @@
 Template.user_sub_menu.active_stories = function(){
-  return Stories.find({owner: Meteor.user().username, status: {$nin : ['new', 'todo']}}).fetch().length;
+  if(Meteor.user().username)
+    return Stories.find({owner: Meteor.user().username, status: {$nin : ['new', 'todo']}}).fetch().length;
 }
