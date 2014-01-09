99d4a31948576ce3317e3766ee807c125807db0b
Darren <darren.royle@oup.com>
2014-01-09 13:32:26 +0000

    Added server auth

--- client/views/layouts/user_sub_menu.js
+++ client/views/layouts/user_sub_menu.js
@@ -1,4 +1,4 @@
 Template.user_sub_menu.active_stories = function(){
-  if(Meteor.user().username)
+  if(Meteor.user())
     return Stories.find({owner: Meteor.user().username, status: {$nin : ['new', 'todo']}}).fetch().length;
 }
